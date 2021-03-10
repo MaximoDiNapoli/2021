8791bf71f8a8ce4f0dc2feab4c5db83bfae1cb43
MaximoDiNapoli <maximodinapolimail@gmail.com>
2021-03-10 20:43:53 -0300

    class Contenido

---
+++ Programación Sobre Redes 4ºCSTC 2021/tp1/clases.ts
@@ -0,0 +1,23 @@
+class Contenido{
+    protected date: Date;
+    protected duracion: number;
+    constructor(duracion: number){
+        this.duracion = duracion;
+    }
+
+    setDate(date: Date){
+        this.date = date;
+    }
+
+    getDate(){
+        return this.date;
+    }
+
+    setDuracion(duracion: number){
+        this.duracion = duracion;
+    }
+
+    getDuracion(){
+        return this.duracion;
+    }
+}
