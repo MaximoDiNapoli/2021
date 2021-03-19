c275a855e5bdbcd0ceb2e029ba461929796750d1
MaximoDiNapoli <maximodinapolimail@gmail.com>
2021-03-19 14:06:09 -0300

    Update Usuario.ts

--- Programación Sobre Redes 4ºCSTC 2021/tp1/clases/Usuario.ts
+++ Programación Sobre Redes 4ºCSTC 2021/tp1/clases/Usuario.ts
@@ -1,6 +1,20 @@
+class Visualizado{
+    private titulo: String;
+    private minuto: number;
+    constructor(titulo: String, minuto: number){
+        this.titulo = titulo;
+        this.minuto = minuto;
+    }
+
+    visto(titulo: String){
+        if(this.minuto == )
+    }
+}
+
 class Usuario{
     private username: String;
     private region: Array<String>;
+    private historial: Array<Visualizado>;
     constructor(username: String, region: Array<String>){
         this.username = username;
         this.region = region;
